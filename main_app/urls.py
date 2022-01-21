1fec6548f707123934d638f087aa3263103db7ed
Jackson Cole <jacksoncole@Jacksons-MacBook-Pro.local>
2022-01-21 11:33:10 -0800

    Complete step 3.1

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.index, name="index"),
     path('add_wiget/', views.WidgetCreate.as_view(), name='add_widget'),
 ]
